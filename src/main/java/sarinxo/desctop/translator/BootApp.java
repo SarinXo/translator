3464c665b8a7754ec2f9bd48298f8efb3c733cc1
SarinXo <egorov.ilya.dev@yandex.ru>
2025-07-01 00:24:34 +0300

    init spring, web и утилитные зависимости

--- src/main/java/sarinxo/desctop/translator/BootApp.java
+++ src/main/java/sarinxo/desctop/translator/BootApp.java
@@ -1,4 +1,12 @@
 package sarinxo.desctop.translator;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class BootApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootApp.class, args);
+    }
 }
