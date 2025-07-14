620f8c714cea8f35381ad59f6375b4b40bcf44ac
Ilya <109240426+SarinXo@users.noreply.github.com>
2025-07-14 10:46:50 +0300

    feat: Добавил checkstyle.xml

--- src/main/java/sarinxo/desctop/translator/BootApp.java
+++ src/main/java/sarinxo/desctop/translator/BootApp.java
@@ -1,7 +1,9 @@
 package sarinxo.desctop.translator;
 
 import javafx.application.Application;
-
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -25,7 +27,18 @@ public class BootApp extends Application {
      */
     @Override
     public void start(Stage stage) throws Exception {
+        stage.setTitle("Главное окно приложения");
 
+        Button btn = new Button("Нажми меня");
+        btn.setOnAction(event -> System.out.println("Кнопка нажата!"));
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+
+        Scene scene = new Scene(root, 400, 300);
+
+        stage.setScene(scene);
+        stage.show();
     }
 
 }
